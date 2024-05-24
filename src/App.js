c2af27f063c8ecb678e8b3af496c34087ce7d4ec
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-24 11:53:29 -0300

    Removendo componente Btn pra outro lugar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import './App.css';
 
+import Btn from './btn/Btn';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -56,20 +58,6 @@ class App extends Component {
           <Btn click={ this.limpar } title={this.state.btnTitle[1]} />
         </div>
       </div>
-    );
-  }
-}
-
-class Btn extends Component{
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <>
-        <a className='btn' onClick={ this.props.click }>{ this.props.title }</a>
-      </>
     );
   }
 }
